2e060f51b818ff5f163251b0817ead82db78a4a1
wujinhjun <wujinhjun@sjtu.edu.cn>
2023-05-04 16:14:18 +0800

    fix: use the hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useContext, useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 
 import Login from "./components/Login";
